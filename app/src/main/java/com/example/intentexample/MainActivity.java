d35ab4221aa50e9dbf156c598497d1b49675f90a
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-04 22:50:11 +0200

    x

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         Intent I = new Intent(this, SecondActivity.class);
+
+        final EditText applesInput = (EditText) findViewById(R.id.applesInput);
+        String userMessage = applesInput.getText().toString();
+        I.putExtra("appleMessage", userMessage);
+
         startActivity(I); // as we only have one Intent object created so far
     }
 }
--- app/src/main/java/com/example/intentexample/SecondActivity.java
+++ app/src/main/java/com/example/intentexample/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.intentexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle applesData = getIntent().getExtras();
+        if(applesData == null){
+            return;
+        }
+        String appleMessage = applesData.getString("appleMessage");
+        final TextView baconText = (TextView) findViewById(R.id.baconText);
+        baconText.setText(appleMessage);
     }
 }
