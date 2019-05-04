ba63b93cbf86e512fbfd74923166b7bec8b0a2fb
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-04 22:00:44 +0200

    creating instance of Intent class by creating an object I in MainActivity

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.intentexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick() {
+        Intent I = new Intent(this, SecondActivity.class);
     }
 }
